9987887957e058f8192fc8e0e0bdc633e8f75b91
caoyanan <2606090119@qq.com>
2016-09-30 10:16:12 +0800

    Initial Commit

---
+++ CustomPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustomPlayer
+//
+//  Created by miniu on 16/9/30.
+//  Copyright © 2016年 mini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
